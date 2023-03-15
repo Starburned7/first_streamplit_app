2d0002f88b5005d5ea6a80496e0ba28a9a9297a7
Zarema <50046942+Starburned7@users.noreply.github.com>
2023-03-15 10:20:36 +0300

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title("My parents' new healthy diner")
-streamlit.header ('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.header('Breakfast Favorites')
 streamlit.text(' Omega 3 & Blueberry Oatmeal')
-streamlit.text(' (3 Kale, Spinach & Rocket Smoothie'
-streamlit.text('4 Hard-Boiled Free-Range Egg'
+streamlit.text(' (3 Kale, Spinach & Rocket Smoothie')
+streamlit.text('4 Hard-Boiled Free-Range Egg')
 streamlit.text('OO Avocado Toast')
