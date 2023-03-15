6eff115128fd3f52615ad17ad0c7fc180af6236e
Zarema <50046942+Starburned7@users.noreply.github.com>
2023-03-15 10:30:33 +0300

    Update streamlit_app.py
    added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 3 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 4 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
