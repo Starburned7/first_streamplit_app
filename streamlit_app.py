0eff0feddc1ee4b7a793b47cef6e098c705bde1a
Zarema <50046942+Starburned7@users.noreply.github.com>
2023-03-15 10:19:40 +0300

    Update streamlit_app.py
    changed lines of text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title("My parents' new healthy diner")
 streamlit.header ('Breakfast Menu')
-streamlit.text ('Omega 3 & Blueberry Oatmeal')
-streamlit.text ('Kale, Spinach & Rocket Smoothie')
-streamit.text ('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text(' Omega 3 & Blueberry Oatmeal')
+streamlit.text(' (3 Kale, Spinach & Rocket Smoothie'
+streamlit.text('4 Hard-Boiled Free-Range Egg'
+streamlit.text('OO Avocado Toast')
