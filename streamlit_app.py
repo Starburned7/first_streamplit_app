24b65bef81c73fcc43ee9e42580c63f7be6325dd
Zarema <50046942+Starburned7@users.noreply.github.com>
2023-03-24 22:19:03 +0300

    Update streamlit_app.py
    fixed formatting errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,8 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 
 
 my_data_rows = my_cur.fetchall()
-streamlit.header ("The fruit load list cont
-streamlit.dataframe (my data rows)
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 
 
 
